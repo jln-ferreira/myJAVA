63fac31ac8174636fc5bf39663fcf017a64f78ad
jln-ferreira <jln-ferreira@hotmail.com>
2019-10-30 09:05:31 -0700

    try_catch

---
+++ src/Try_Catch/Try_Catch1.java
@@ -0,0 +1,15 @@
+package Try_Catch;
+
+public class Try_Catch1 {
+    public static void main(String[] args) {
+
+        
+    }
+
+    //some methods
+    static double divide(double a, double b){
+        return a/b;
+    }
+}
+
+
